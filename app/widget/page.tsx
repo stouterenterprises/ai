700b366ffd96fbad71d4530aa47679d480f6ec37
stouterenterprises <blstouter93@gmail.com>
2026-01-15 01:37:52 -0500

    Add widget snippet copy button

--- app/widget/page.tsx
+++ app/widget/page.tsx
@@ -1,4 +1,5 @@
 import WidgetPreview from "@/components/widget-preview";
+import WidgetSnippet from "@/components/widget-snippet";
 
 export default function WidgetPage() {
   return (
@@ -10,9 +11,7 @@ export default function WidgetPage() {
           Add this snippet to any site to load the Nimbus messenger widget. The widget
           connects to the real-time messaging service for chat and uses department routing rules.
         </p>
-        <pre>
-          {`<script src="https://YOUR_DOMAIN/widget.js" data-business="YOUR_BUSINESS_ID"></script>`}
-        </pre>
+        <WidgetSnippet />
       </section>
       <section>
         <WidgetPreview />
